fa2a1f69a05d4f8042fc27cf30f87d279cb2bcc7
Harsh-Srivastava <1by23ai058@bmsit.in>
2025-02-19 00:45:14 +0530

    added features page

--- grindjeet/src/App.jsx
+++ grindjeet/src/App.jsx
@@ -2,6 +2,7 @@ import  Navbar  from "./Components/Navbar";
 import './App.css';
 import MainPage from "./Components/HeroPage";
 import About from "./Components/AboutPage";
+import Features from "./Components/Features";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Navbar />
             <MainPage />
             <About />
+            <Features />
         </>
     );
 }
---
+++ grindjeet/src/Components/Features.jsx
@@ -0,0 +1,40 @@
+export default function Features(){
+  return <>
+    <div className="features">
+      <div className="f1">
+        <h1> Why Choose</h1>
+        <h1><span>GrindBook</span></h1>
+      </div>
+      <div className="cards">
+        <div className="row-1">
+          <div className="card-1 c">
+            <div className="subheading"><h3>Revise Effectively</h3></div>
+            <div className="feat"><p>Revise efffetively with help of questions, correct answer and what you learnt from that question at one place with help of GrindBook.</p></div>
+          </div>
+          <div className="card-2">
+            <div className="subheading"><h3>Save Questions</h3></div>
+            <div className="feat"><p>Easily save direct links to questions or problems from any platform. Keep all your references organized in one place for quick access and review.</p></div>
+          </div>
+          <div className="card-3">
+            <div className="subheading"><h3>Track your Progress</h3></div>
+            <div className="feat"><p>Document key takeaways and lessons learned from each question. Reflect on your problem-solving approaches and track your growth over time.</p></div>
+          </div>
+        </div>
+        <div className="row-2">
+          <div className="card-4">
+            <div className="subheading"><h3>Revise By Topics</h3></div>
+            <div className="feat"><p>Categorize questions by topics or subjects for easy filtering and searching. Build a personal knowledge map to visualize your expertise.</p></div>
+          </div>
+          <div className="card-5">
+            <div className="subheading"><h3>Smart Notes</h3></div>
+            <div className="feat"><p>Add flexible notes to each question, including solutions, explanations, and custom tags. Set reminders to revisit important concepts.</p></div>
+          </div>
+          <div className="card-6">
+            <div className="subheading"><h3>Search and Filter</h3></div>
+            <div className="feat"><p>Quickly find questions and notes using advanced search and filtering options. Sort by topic to focus on what matters most.</p></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+}
